74534165e84d6a0259da9e1039758780ee2d5c71
fnobi <shin@fnobi.com>
2012-10-30 07:11:55 +0900

    first commit (new rails project).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Script2012fRailssample::Application.config.secret_token = '7cf49215e51e592042f85b98d532c05ea02de1d5c9e06702dc8566073d796504888d32d60c8732271798cb0a1c6354c707a7555e4e0127df27c57213fd3256f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Script2012fRailssample::Application.config.session_store :cookie_store, key: '_script2012f-railssample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Script2012fRailssample::Application.config.session_store :active_record_store
